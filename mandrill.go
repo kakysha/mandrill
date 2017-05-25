e5e0ae985d410eef0919138f71c5315b213ff6c7
Anthony Heidenreich <anthony.heidenreich@kasita.com>
2017-05-25 10:38:03 -0500

    KSW-98 #comment fixed the addVariables

--- mandrill.go
+++ mandrill.go
@@ -128,6 +128,8 @@ type Message struct {
 	GlobalMergeVars []*Variable `json:"global_merge_vars,omitempty"`
 	// per-recipient merge variables, which override global merge variables with the same name.
 	MergeVars []*RcptMergeVars `json:"merge_vars,omitempty"`
+	// temporary hold for recipient merge variables
+	recipientMergeVarMap map[string]map[string]interface{}
 	// an array of string to tag the message with. Stats are accumulated using tags, though we only store the first 100 we see, so this should not be unique or change frequently. Tags should be 50 characters or less. Any tags starting with an underscore are reserved for internal use and will cause errors.
 	Tags []string `json:"tags,omitempty"`
 	// the unique id of a subaccount for this message - must already exist or will fail with an error
@@ -298,6 +300,7 @@ func (c *Client) Ping() (pong string, err error) {
 
 // MessagesSend sends a message via an API client
 func (c *Client) MessagesSend(message *Message) (responses []*MessagesResponse, err error) {
+	message.MapVariablesToRecipient()
 
 	var data struct {
 		Key     string   `json:"key"`
@@ -321,6 +324,7 @@ func (c *Client) MessagesSend(message *Message) (responses []*MessagesResponse, 
 
 // MessagesSendTemplate sends a message using a Mandrill template
 func (c *Client) MessagesSendTemplate(message *Message, templateName string, contents interface{}) (responses []*MessagesResponse, err error) {
+	message.MapVariablesToRecipient()
 
 	var data struct {
 		Key             string      `json:"key"`
@@ -518,15 +522,29 @@ func (m *Message) AddBCC(email, name string) {
 
 // AddVariable adds a map of values to the message
 func (m *Message) AddVariable(email, key string, value interface{}) {
-	values := make(map[string]interface{})
-	values[key] = value
-	m.AddVariables(email, values)
+	if nil == m.recipientMergeVarMap {
+		m.recipientMergeVarMap = make(map[string]map[string]interface{})
+	}
+	if _, ok := m.recipientMergeVarMap[email]; !ok {
+		m.recipientMergeVarMap[email] = make(map[string]interface{})
+	}
+	m.recipientMergeVarMap[email][key] = value
 }
 
 // AddVariables adds a map of values to the message
 func (m *Message) AddVariables(email string, values map[string]interface{}) {
 	recipientMergeVars := MapToRecipientVars(email, values)
 	m.MergeVars = append(m.MergeVars, recipientMergeVars)
+}
+
+// MapVariablesToRecipient maps the variables added for a recipient to the MergeVars
+func (m *Message) MapVariablesToRecipient() {
+	if nil == m.recipientMergeVarMap {
+		return
+	}
+	for email, values := range m.recipientMergeVarMap {
+		m.AddVariables(email, values)
+	}
 }
 
 func (c *Client) sendMessagePayload(data interface{}, path string) (responses []*MessagesResponse, err error) {
